2b466fae56c879e54260b308722e197e4dce24e9
senith <senithdakshina@gmail.com>
2025-12-06 16:11:09 +0530

    page.tsx error

--- app/(auth)/sign_up/page.tsx
+++ app/(auth)/sign_up/page.tsx
@@ -2,7 +2,7 @@ import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const page = () => {
-  return <AuthForm type ="sign-in"></AuthForm>
+  return <AuthForm type="sign-up"></AuthForm>
 }
 
 export default page
