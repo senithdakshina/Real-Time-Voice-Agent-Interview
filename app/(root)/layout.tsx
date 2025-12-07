48851f17fcb90b8dc0b4756b6d23e46fecaf8c6c
senith <senithdakshina@gmail.com>
2025-12-07 12:45:36 +0530

    sign up

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,17 @@
+import { Mona_Sans } from "next/font/google";
 import React, { ReactNode } from 'react'
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
 
 const Rootlayout = ({children} :{children: ReactNode}) => {
   return (
-    <div>
-      
-    </div>
+    <html lang='en' className='dark'>
+    <body className={`${monaSans.className} antialiased pattern`}>
+      {children}
+    </body>
+    </html>
   )
 }
 
