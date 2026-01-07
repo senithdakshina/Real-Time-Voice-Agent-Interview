51367f61ee5a653422fc82a7347afe66a4f28686
senith <senithdakshina@gmail.com>
2026-01-07 20:04:15 +0530

    interview page correted

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,30 +1,39 @@
-import React from 'react'
-import Image from 'next/image'
-const Agent = ({userName} : AgentProps) => {
-
+import React from "react";
+import Image from "next/image";
+const Agent = ({ userName }: AgentProps) => {
   const isSpeaking = true;
   return (
     <>
-<div className='call-view'>
-      <div className='card-interviewer'>
-        <div className='avatar'> 
-          <Image src="/ai-avatar.png" alt='vapi' width={65} height={54} className='object-cover'></Image>
-          {isSpeaking && <span className='animate-speak'></span>}
+      <div className="call-view">
+        <div className="card-interviewer">
+          <div className="avatar">
+            <Image
+              src="/ai-avatar.png"
+              alt="vapi"
+              width={65}
+              height={54}
+              className="object-cover"
+            ></Image>
+            {isSpeaking && <span className="animate-speak"></span>}
+          </div>
+          <h3>AI Interviewer</h3>
         </div>
-        <h3>AI Interviewer</h3>
-      </div>
 
-      <div className='card-border'>
-        <div className='card-content'></div>
-          <Image src="/user-avatar.png" alt='user avatar' width={540} height={540} className='rounded-full object-cover size-[120px]'></Image>
+        <div className="card-border">
+          <div className="card-content">
+          <Image
+            src="/user-avatar.png"
+            alt="user avatar"
+            width={540}
+            height={540}
+            className="rounded-full object-cover size-[120px]"
+          ></Image>
           <h3>{userName}</h3>
+          </div>
+        </div>
       </div>
-      
-    </div>
-  
     </>
-  )  
-    
-}
+  );
+};
 
-export default Agent
+export default Agent;
